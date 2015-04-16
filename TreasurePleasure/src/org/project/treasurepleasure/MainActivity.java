3b692f640775277203aa0454d5300d69c8434962
cristina-brinza <cristinaioana.brinza@gmail.com>
2015-04-17 02:00:36 +0300

    added code for file uploading

--- TreasurePleasure/src/org/project/treasurepleasure/MainActivity.java
+++ TreasurePleasure/src/org/project/treasurepleasure/MainActivity.java
@@ -1,7 +1,10 @@
 package org.project.treasurepleasure;
 
-import android.support.v7.app.ActionBarActivity;
+import org.project.garbage.UploadFileActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -19,6 +22,10 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        
+        Intent intent = new Intent(this, UploadFileActivity.class);
+        startActivity(intent);
+        
         return true;
     }
 
