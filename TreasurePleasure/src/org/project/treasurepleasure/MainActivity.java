dd1375f44d4b2d790fc541938fce5f50cfe080ee
cristina-brinza <cristinaioana.brinza@gmail.com>
2015-04-17 21:25:59 +0300

    added .gitignore

--- TreasurePleasure/src/org/project/treasurepleasure/MainActivity.java
+++ TreasurePleasure/src/org/project/treasurepleasure/MainActivity.java
@@ -1,6 +1,5 @@
 package org.project.treasurepleasure;
 
-
 import org.project.garbage.UploadFileActivity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,45 +8,45 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-
 public class MainActivity extends ActionBarActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    public void joinGame(View view) {
-    		Intent intent = new Intent(this, JoinGameActivity.class);
-    		startActivity(intent);
-    }
-    
-    public void addGame(View view) {
-    		Intent intent = new Intent(this, AddGameActivity.class);
+	public void joinGame(View view) {
+		Intent intent = new Intent(this, JoinGameActivity.class);
 		startActivity(intent);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-    	
-    	getMenuInflater().inflate(R.menu.main, menu);
-        
-      /*  Intent intent = new Intent(this, UploadFileActivity.class);
-        startActivity(intent);
-        */
-        return true;
-    }
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        	// Handle action bar item clicks here. The action bar will
-    		// automatically handle clicks on the Home/Up button, so long
-    		// as you specify a parent activity in AndroidManifest.xml.
-        	int id = item.getItemId();
-        	if (id == R.id.action_settings) {
-        		return true;
-        	}
-        	return super.onOptionsItemSelected(item);
-    }
+	public void addGame(View view) {
+		Intent intent = new Intent(this, AddGameActivity.class);
+		startActivity(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+
+		getMenuInflater().inflate(R.menu.main, menu);
+
+		Intent intent = new Intent(this, UploadFileActivity.class);
+		startActivity(intent);
+ 
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
 }
