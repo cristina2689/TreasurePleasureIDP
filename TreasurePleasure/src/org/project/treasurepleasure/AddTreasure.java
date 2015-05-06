6ab4f8fc206f5972bbe818243b504f0cca3a6103
cristina-brinza <cristinaioana.brinza@gmail.com>
2015-05-07 01:09:47 +0300

    #6 photo upload for treasure

--- TreasurePleasure/src/org/project/treasurepleasure/AddTreasure.java
+++ TreasurePleasure/src/org/project/treasurepleasure/AddTreasure.java
@@ -7,23 +7,17 @@ import static org.project.treasurepleasure.Utilities.GO_BACK_TREASURE_URL;
 import static org.project.treasurepleasure.Utilities.UPLOAD_FILE_PATH;
 import static org.project.treasurepleasure.Utilities.game_id;
 
-import java.io.BufferedWriter;
-import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.project.databaseutil.AddTreasureConnectDB;
 
 import android.app.Activity;
@@ -34,7 +28,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore.MediaColumns;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -56,6 +49,7 @@ import android.widget.TextView;
  * apoi o sa se intoarca la parinte sa adauge si alte comori.
  */
 
+@SuppressWarnings("deprecation")
 public class AddTreasure extends ActionBarActivity {
 
 	double latitude = 45.46363, longitude = 54.43636;
@@ -72,16 +66,10 @@ public class AddTreasure extends ActionBarActivity {
 
 	public void addTreasure(View view) {
 		String hint = ((EditText) findViewById(R.id.hintEditText)).getText().toString();
-		String treasure_url = Utilities.SERVER_URL + Utilities.game_title + "_" + Utilities.game_id 
-				+ "/" + selectedImagePath.substring(selectedImagePath.lastIndexOf('/') + 1);
-				
+		String treasure_url = Utilities.SERVER_URL + Utilities.game_title + "_" + Utilities.game_id + "/" + selectedImagePath.substring(selectedImagePath.lastIndexOf('/') + 1);
+
 		// add in DB
-		new AddTreasureConnectDB(this, AddTreasure.this).execute(
-				String.valueOf(latitude), 
-				String.valueOf(longitude),
-				treasure_url, 
-				String.valueOf(game_id), 
-				hint);
+		new AddTreasureConnectDB(this, AddTreasure.this).execute(String.valueOf(latitude), String.valueOf(longitude), treasure_url, String.valueOf(game_id), hint);
 
 		// send back info
 		Intent resultIntent = new Intent();
@@ -115,7 +103,7 @@ public class AddTreasure extends ActionBarActivity {
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
-						 new UploadPhoto().execute(selectedImagePath);
+						new UploadPhoto().execute(selectedImagePath);
 					}
 				});
 			}
@@ -124,125 +112,41 @@ public class AddTreasure extends ActionBarActivity {
 	}
 
 	class UploadPhoto extends AsyncTask<String, Void, String> {
-
+		
 		@Override
 		protected String doInBackground(String... params) {
-			
 			uploadPhoto(params[0]);
-			
+
 			return null;
 		}
-		
 	}
-	
+
 	private void uploadPhoto(String toUploadFilePath) {
-		int serverResponseCode = 0;
-		HttpURLConnection conn = null;
-		DataOutputStream dos = null;
 		String uploadFileName = toUploadFilePath.substring(toUploadFilePath.lastIndexOf('/') + 1);
 		String uploadDirectory = Utilities.game_title + "_" + Utilities.game_id;
-		String lineEnd = "\r\n";
-		String twoHyphens = "--";
-		String boundary = "*****";
-		int bytesRead, bytesAvailable, bufferSize;
-		byte[] buffer;
-		int maxBufferSize = 1 * 1024 * 1024;
 		File sourceFile = new File(toUploadFilePath);
+		String result;
 
-		if (!sourceFile.isFile()) {
-			Log.e("uploadFile", "Source File not exist :" + UPLOAD_FILE_PATH + "" + uploadFileName);
-		} else {
-			try {
-				System.setProperty("http.keepAlive", "false");
+		try {
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpPost httpPost = new HttpPost(UPLOAD_FILE_PATH);
 
-				// open a URL connection to the Servlet
-				FileInputStream fileInputStream = new FileInputStream(sourceFile);
-				URL url = new URL(UPLOAD_FILE_PATH);
+			MultipartEntityBuilder entityBuilder = MultipartEntityBuilder.create();
+			entityBuilder.setMode(HttpMultipartMode.BROWSER_COMPATIBLE);
 
-				// Open a HTTP connection to the URL
-				conn = (HttpURLConnection) url.openConnection();
-				conn.setDoInput(true); // Allow Inputs
-				conn.setDoOutput(true); // Allow Outputs
-				conn.setUseCaches(false); // Don't use a Cached Copy
-				conn.setRequestMethod("POST");
-				conn.setRequestProperty("Connection", "Keep-Alive");
-				conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-				conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-				conn.setRequestProperty("uploaded_file", uploadFileName);
-				conn.setRequestProperty("upload_directory", uploadDirectory);
+			entityBuilder.addTextBody("upload_directory", uploadDirectory);		
+			entityBuilder.addBinaryBody("uploaded_file", sourceFile, ContentType.create("image/jpeg"), uploadFileName);
 
-				List<NameValuePair> params = new ArrayList<NameValuePair>();
-				params.add(new BasicNameValuePair("upload_directory", uploadDirectory));								
-				dos = new DataOutputStream(conn.getOutputStream());
-				
-				dos.writeBytes(getQuery(params));
-				
-				dos.writeBytes(twoHyphens + boundary + lineEnd);
-//				dos.writeBytes("Content-Disposition: form-data; name=\"upload_directory\"" + lineEnd + uploadDirectory + lineEnd);
-				dos.writeBytes("Content-Disposition: form-data; name=\"uploaded_file\";filename=\"" + uploadFileName + "\"" + lineEnd);
-
-				dos.writeBytes(lineEnd);
-
-				// create a buffer of maximum size
-				bytesAvailable = fileInputStream.available();
-
-				bufferSize = Math.min(bytesAvailable, maxBufferSize);
-				buffer = new byte[bufferSize];
-
-				// read file and write it into form...
-				bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-				while (bytesRead > 0) {
-
-					dos.write(buffer, 0, bufferSize);
-					bytesAvailable = fileInputStream.available();
-					bufferSize = Math.min(bytesAvailable, maxBufferSize);
-					bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-				}
-
-				// send multipart form data necesssary after file data...
-				dos.writeBytes(lineEnd);
-				dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-				// Responses from the server (code and message)
-				serverResponseCode = conn.getResponseCode();
-				String serverResponseMessage = conn.getResponseMessage();
-
-				Log.i("uploadFile", "HTTP Response is : " + serverResponseMessage + ": " + serverResponseCode);
-
-				if (serverResponseCode == 200) {
-
-					// runOnUiThread(new Runnable() {
-					// public void run() {
-					//
-					// String msg =
-					// "File Upload Completed.\n\n See uploaded file here : \n\n"
-					// + UPLOAD_FILE_PATH + uploadFileName;
-					//
-					// messageText.setText(msg);
-					// Toast.makeText(UploadToServer.this,
-					// "File Upload Complete.", Toast.LENGTH_SHORT).show();
-					// }
-					// });
-				}
-
-				// close the streams //
-				fileInputStream.close();
-				dos.flush();
-				dos.close();
-
-			} catch (MalformedURLException ex) {
-
-				Log.e("Upload file to server", "error: " + ex.getMessage(), ex);
-			} catch (IOException e) {
-
-				Log.e("Upload file to server Exception", "Exception : " + e.getMessage(), e);
-			}
-			// dialog.dismiss();
-			// return serverResponseCode;
-
-		} // End else block
+			HttpEntity entity = entityBuilder.build();
+			httpPost.setEntity(entity);
+			HttpResponse response = httpClient.execute(httpPost);
+			HttpEntity httpEntity = response.getEntity();
+			result = EntityUtils.toString(httpEntity);
+			
+			android.util.Log.i("result", result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	// get image path
@@ -258,24 +162,4 @@ public class AddTreasure extends ActionBarActivity {
 		return imagePath;
 	}
 
-	private String getQuery(List<NameValuePair> params) throws UnsupportedEncodingException
-	{
-	    StringBuilder result = new StringBuilder();
-	    boolean first = true;
-
-	    for (NameValuePair pair : params)
-	    {
-	        if (first)
-	            first = false;
-	        else
-	            result.append("&");
-
-	        result.append(URLEncoder.encode(pair.getName(), "UTF-8"));
-	        result.append("=");
-	        result.append(URLEncoder.encode(pair.getValue(), "UTF-8"));
-	    }
-
-	    return result.toString();
-	}
-	
 }
